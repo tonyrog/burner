709c04abc85a2d216ae232c107e1fbfe47a6a0ed
Kim Rogvall <kim@rogvall.se>
2025-06-01 22:45:59 +0200

    Lade till ändringar i styles.css tillsammans med burner.shtml och script.js

--- www/html/js/script.js
+++ www/html/js/script.js
@@ -1,4 +1,3 @@
-
 function deleteRow(button) {
   const item = button.closest('.burner-item');
   if (item) item.remove();
@@ -17,50 +16,42 @@ document.addEventListener("DOMContentLoaded", () => {
     const li = document.createElement("li");
     li.className = "burner-item";
 
-    
-  
-  
-  
-  li.innerHTML = `
-    <div>
-      <div class="alias-input">
-        <span class="prefix">${conf.username}.</span>
-        <input type="text" value="${entry.alias}" placeholder="alias" />
-        <span class="suffix">@${conf.domain}</span>
+    li.innerHTML = `
+      <div>
+        <div class="alias-input">
+          <span class="prefix">${conf.username}.</span>
+          <input type="text" value="${entry.alias}" placeholder="alias" />
+          <span class="suffix">@${conf.domain}</span>
+        </div>
+        <div class="label">BURNER ADDRESS</div>
       </div>
-      <div class="label">BURNER ADDRESS</div>
-    </div>
-    <div>
-      <div class="description-block">
-        <input type="text" value="${entry.comment}" />
-        <button class="icon-button">✕</button>
-        <button class="icon-button confirm">✔</button>
+      <div>
+        <div class="description-block">
+          <input type="text" value="${entry.description}" />
+          <button class="icon-button">✕</button>
+          <button class="icon-button confirm">✔</button>
+        </div>
+        <div class="label">DESCRIPTION</div>
       </div>
-      <div class="label">DESCRIPTION</div>
-    </div>
-    <div>
-      <div class="value">${entry.date}</div>
-      <div class="label">CREATED AT</div>
-    </div>
-    <div>
-      <div class="value">${entry.forwarded}</div>
-      <div class="label">FORWARDED</div>
-    </div>
-    <div class="delete-burn-wrap">
-      <div class="delete-wrap">
-        <button class="icon-button" onclick="deleteRow(this)" title="Delete">✕</button>
-        <div class="label">DELETE</div>
+      <div>
+        <div class="value">${entry.date}</div>
+        <div class="label">CREATED AT</div>
       </div>
-      <div class="burn-wrap">
-        <img src="img/flaming_skull_icon.png" alt="burn icon" onclick="deleteRow(this)" style="cursor:pointer;" title="Burn" />
-        <div class="label">BURN</div>
+      <div>
+        <div class="value">${entry.forwarded}</div>
+        <div class="label">FORWARDED</div>
       </div>
-    </div>
-  `;
-
-
-
-
+      <div class="delete-burn-wrap">
+        <div class="delete-wrap">
+          <button class="icon-button" onclick="deleteRow(this)" title="Delete">✕</button>
+          <div class="label">DELETE</div>
+        </div>
+        <div class="burn-wrap">
+          <img src="img/flaming_skull_icon.png" alt="burn icon" onclick="deleteRow(this)" style="cursor:pointer;" title="Burn" />
+          <div class="label">BURN</div>
+        </div>
+      </div>
+    `;
 
     list.appendChild(li);
   });
